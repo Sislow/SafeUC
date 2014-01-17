9cdfbf8513664cedb2a5c6537716a76585905f2f
Ohardwick <hardwiog@mail.uc.edu>
2014-01-17 10:53:08 -0500

    First Commit

---
+++ GregsList/src/hardwiog/uc/gregslist/Open.java
@@ -0,0 +1,22 @@
+package hardwiog.uc.gregslist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Open extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_open);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.open, menu);
+		return true;
+	}
+
+}
